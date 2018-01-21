6c32ea6235e36f68f12ea95702e283af5d4c84b9
Egor <Egorator1996@gmail.com>
2018-01-21 17:00:50 +0200

    Fix build.

---
+++ src/main/java/ua/training/entity/proxy/UserProxy.java
@@ -0,0 +1,29 @@
+package ua.training.entity.proxy;
+
+import ua.training.dao.RoleDao;
+import ua.training.dao.factory.DaoFactory;
+import ua.training.dao.factory.DataSourceFactory;
+import ua.training.entity.Role;
+import ua.training.entity.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class UserProxy extends User {
+    @Override
+    public Role getRole() {
+        if (super.getRole() == null) {
+            DataSource dataSource = DataSourceFactory.getInstance().getDataSource();
+            try (Connection connection = dataSource.getConnection()) {
+                DaoFactory daoFactory = DaoFactory.getDaoFactory(connection);
+                RoleDao roleDao = daoFactory.createRoleDao();
+                return roleDao.findByUser(getId());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        return super.getRole();
+    }
+}
